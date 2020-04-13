d09134faaec02ca6fa4f8d0cbcbd5463241b17dc
Tony Greening <tgreening@yahoo.com>
2020-04-13 18:56:54 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# BeerThermometer-Android
+ Android app that accompanies the ESP32 probe project - https://github.com/tgreening/beer_temperature_esp32
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { ChartsModule } from 'ng2-charts';
 
 import { HomePage } from './home.page';
 
@@ -11,6 +12,7 @@ import { HomePage } from './home.page';
     CommonModule,
     FormsModule,
     IonicModule,
+    ChartsModule,
     RouterModule.forChild([
       {
         path: '',
